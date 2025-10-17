0f5ed960b2a2212d7f2f34ea13bc9a0b68244df5
nad <nadeline.yen@students.mikroskil.ac.id>
2025-10-17 20:09:05 +0700

    perbaikan login

--- book-library/src/pages/Login/Login.jsx
+++ book-library/src/pages/Login/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import "./Login.css";
-import { useNavigate, useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
   const [username, setUsername] = useState("");
